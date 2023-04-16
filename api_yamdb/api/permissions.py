6222d7a9b51097d4ac35a0138a189319fdf109ad
MindlinKS@yandex.ru <MindlinKS@yandex.ru>
2023-04-16 21:56:22 +0300

    final

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -17,18 +17,14 @@ class IsAdminSuperuserUserOrReadOnly(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (
-                request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
                 or request.user.role in ['admin', 'moderator']
                 or request.user.is_superuser
-                or obj.author == request.user
-        )
+                or obj.author == request.user)
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
